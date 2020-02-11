7c76a92180c5c225614d62a4955612e93d670c75
Eddie Butler <eddie.butler@perficient.com>
2020-02-11 10:08:59 -0500

    initial commit

---
+++ ADI_UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ADI_UI.MvcApplication" Language="C#" %>
---
+++ ADI_UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ADI_UI.Startup))]
+namespace ADI_UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
